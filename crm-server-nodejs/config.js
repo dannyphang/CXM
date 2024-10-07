be4a730d2d2781aa50c132667f862e3c931f36df
DannyPhang <68151810+dannyphang@users.noreply.github.com>
2024-10-07 14:19:33 +0800

    update firebase serviec key

--- crm-server-nodejs/config.js
+++ crm-server-nodejs/config.js
@@ -14,6 +14,17 @@ const {
   FIREBASE_MESSAGING_SENDER_ID,
   FIREBASE_APP_ID,
   FIREBASE_MEASUREMENT_ID,
+  type,
+  project_id,
+  private_key_id,
+  private_key,
+  client_email,
+  client_id,
+  auth_uri,
+  token_uri,
+  auth_provider_x509_cert_url,
+  client_x509_cert_url,
+  universe_domain,
 } = process.env;
 
 assert(PORT, "Port is required");
@@ -31,5 +42,18 @@ export default {
     messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
     appId: FIREBASE_APP_ID,
     measurementId: FIREBASE_MEASUREMENT_ID,
+  },
+  serviceAcc: {
+    type: type,
+    project_id: project_id,
+    private_key_id: private_key_id,
+    private_key: private_key.replace(/\\n/g, "\n"),
+    client_email: client_email,
+    client_id: client_id,
+    auth_uri: auth_uri,
+    token_uri: token_uri,
+    auth_provider_x509_cert_url: auth_provider_x509_cert_url,
+    client_x509_cert_url: client_x509_cert_url,
+    universe_domain: universe_domain,
   },
 };
