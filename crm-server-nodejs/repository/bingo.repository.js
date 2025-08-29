8258a70659b962454cbf7b3e95996063783b0b5d
DannyPhang <dannyphang64@gmail.com>
2025-08-29 15:33:07 +0800

    refactor: streamline promise handling in bingo repository functions

--- crm-server-nodejs/repository/bingo.repository.js
+++ crm-server-nodejs/repository/bingo.repository.js
@@ -4,89 +4,80 @@ const bingoTable = "bingo";
 const bingoPlayerTable = "bingoPlayer";
 
 function getBingoData() {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const { data, error } = await supabase.from(bingoTable).select("*");
-      if (error) {
-        if (!data) {
-          reject(`Bingo data not found`);
+    return new Promise(async (resolve, reject) => {
+        try {
+            const { data, error } = await supabase.from(bingoTable).select("*");
+            if (error) {
+                if (!data) {
+                    reject(`Bingo data not found`);
+                }
+                reject(error);
+            } else {
+                resolve(data);
+            }
+        } catch (error) {
+            reject(error);
         }
-        reject(error);
-      } else {
-        resolve(data);
-      }
-    } catch (error) {
-      reject(error);
-    }
-  });
+    });
 }
 
 function getBingoDataByUid(uid) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const { data, error } = await supabase.from(bingoTable).select("*").eq("uid", uid).single();
-      if (error) {
-        console.log(error);
-        if (!data) {
-          reject(`Bingo data not found`);
+    return new Promise(async (resolve, reject) => {
+        try {
+            const { data, error } = await supabase.from(bingoTable).select("*").eq("uid", uid).eq("statusId", 1).single();
+            if (error) {
+                console.log(error);
+                if (!data) {
+                    reject(`Bingo data not found`);
+                }
+                reject(error);
+            } else {
+                resolve(data);
+            }
+        } catch (error) {
+            reject(error);
         }
-        reject(error);
-      } else {
-        resolve(data);
-      }
-    } catch (error) {
-      reject(error);
-    }
-  });
+    });
 }
 
 function createUser(data) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const res = await supabase.from(bingoPlayerTable).insert(data).select().single();
-      resolve(res.data);
-    } catch (error) {
-      reject(error);
-    }
-  });
+    return new Promise(async (resolve, reject) => {
+        try {
+            const res = await supabase.from(bingoPlayerTable).insert(data).select().single();
+            resolve(res.data);
+        } catch (error) {
+            reject(error);
+        }
+    });
 }
 
 function updateUser(user) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const res = await supabase
-        .from(bingoPlayerTable)
-        .update(user)
-        .eq("uid", user.uid)
-        .select()
-        .single();
-      resolve(res.data);
-    } catch (error) {
-      reject(error);
-    }
-  });
+    return new Promise(async (resolve, reject) => {
+        try {
+            const res = await supabase.from(bingoPlayerTable).update(user).eq("uid", user.uid).eq("statusId", 1).select().single();
+            resolve(res.data);
+        } catch (error) {
+            reject(error);
+        }
+    });
 }
 
 function getUser(name) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const { data, error } = await supabase
-        .from(bingoPlayerTable)
-        .select("*")
-        .eq("name", name)
-        .single();
-      if (error) {
-        if (!data) {
-          reject("User not found");
+    return new Promise(async (resolve, reject) => {
+        try {
+            const { data, error } = await supabase.from(bingoPlayerTable).select("*").eq("name", name).eq("statusId", 1).single();
+            if (error) {
+                if (!data) {
+                    reject("User not found");
+                }
+                reject(error);
+            } else {
+                resolve(data);
+            }
+        } catch (error) {
+            reject(error);
         }
-        reject(error);
-      } else {
-        resolve(data);
-      }
-    } catch (error) {
-      reject(error);
-    }
-  });
+    });
 }
 
 export { getBingoData, getBingoDataByUid, createUser, updateUser, getUser };
