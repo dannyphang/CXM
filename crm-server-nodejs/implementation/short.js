123ec20e95f60d46d00db5ffdd509be67b510d12
danny.phang <danny.phang@simplr.com.my>
2026-01-02 17:38:28 +0800

    feat: add password handling to createShortenUrl function

--- crm-server-nodejs/implementation/short.js
+++ crm-server-nodejs/implementation/short.js
@@ -2,20 +2,22 @@ import * as shortRepo from "../repository/short.repository.js";
 import * as func from "../shared/function.js";
 import * as envConfig from "../configuration/envConfig.js";
 import * as constant from "../shared/constant.js";
+import bcrypt from "bcryptjs";
 
-function createShortenUrl({ url, expiry }) {
-    return new Promise((resolve, reject) => {
+function createShortenUrl({ url, expiry, password }) {
+    return new Promise(async (resolve, reject) => {
         try {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+            const randomString = func.generateRandomString(10);
+
             let obj = {
                 originalUrl: url,
-                shortUrl: "",
-                path: "",
+                shortUrl: `${envConfig.clientUrl}/su/${randomString}`,
+                path: randomString,
                 expiry: expiry,
-            };
-
-            // generate a random short 10 alphanumeric string
-            obj.path = func.generateRandomString(10);
-            obj.shortUrl = `${envConfig.clientUrl}/su/${obj.path}`;
+                password: hashedPassword || null,
+            };            
 
             shortRepo
                 .createShortUrl({ url: obj })
